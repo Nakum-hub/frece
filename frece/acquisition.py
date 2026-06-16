ce30e23d62a0653dd6b5809a4f1a5bb831b96dbe
Naveen T S <tsnaveen551@gmail.com>
2026-06-16 22:40:48 +0530

    Relicense header: acquisition (MIT -> proprietary)

--- frece/acquisition.py
+++ frece/acquisition.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 FRECE Contributors. Licensed under the MIT License.
+# Copyright (c) 2025 Nakum-hub. All rights reserved. Proprietary and confidential. Unauthorized use, copying, modification, or distribution is prohibited.
 """Evidence acquisition with write-block enforcement."""
 
 import hashlib
